b4e319f24844cb743e620cb594dc585d7bcd4259
Bibek Lama <netshering@gmail.com@users.noreply.github.com>
2020-09-02 11:22:35 +0200

    Cross Origin Update

--- pgsql-micro-service/src/main/java/fr/epita/pgsql/launcher/ProfileController.java
+++ pgsql-micro-service/src/main/java/fr/epita/pgsql/launcher/ProfileController.java
@@ -2,6 +2,7 @@ package fr.epita.pgsql.launcher;
 
 import javax.inject.Inject;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,21 +22,25 @@ public class ProfileController {
 	@Inject
 	PgsqlProfileDAO dao;
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value="/profiles/{id}")
 	public Profile getProfileById(@PathVariable long id) throws PgsqlProfileBusinessException, PgsqlAddressBusinessException {
 		return dao.getProfileById(id);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@PostMapping(value="/profiles/{userId}")
 	public Profile addProfle(@RequestBody Profile profile, @PathVariable long userId) throws PgsqlProfileBusinessException, PgsqlAddressBusinessException {
 		return dao.addProfile(profile, userId);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@PutMapping(value="/profiles/{id}")
 	public Profile updateProfle(@RequestBody Profile profile, @PathVariable long id) throws PgsqlProfileBusinessException, PgsqlAddressBusinessException {
 		return dao.updateProfile(profile, id);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@DeleteMapping(value="/profiles/{id}")
 	public Profile deleteProfle(@PathVariable long id) throws PgsqlProfileBusinessException, PgsqlAddressBusinessException {
 		return dao.deleteProfile(id);
--- pgsql-micro-service/src/main/java/fr/epita/pgsql/launcher/UserController.java
+++ pgsql-micro-service/src/main/java/fr/epita/pgsql/launcher/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,31 +26,37 @@ public class UserController {
 	@Inject 
 	PgsqlUserDAO dao;
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value="/users")
 	public List<User> listUsers() throws PgsqlUserBusinessException{
 		return dao.listUsers();
 	}
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value="/users/{id}")
 	public User getUserById(@PathVariable long id) throws PgsqlUserBusinessException, PgsqlProfileBusinessException, PgsqlAddressBusinessException{
 		return dao.getUserById(id);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@PostMapping(value="/users")
 	public User addUser(@RequestBody User user) throws PgsqlUserBusinessException, PgsqlProfileBusinessException, PgsqlAddressBusinessException{
 		return dao.addUser(user);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@PutMapping(value="/users/{id}")
 	public User updateUser(@RequestBody User user, @PathVariable long id) throws PgsqlUserBusinessException, PgsqlProfileBusinessException, PgsqlAddressBusinessException {
 		return dao.updateUser(user, id);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@DeleteMapping(value="/users/{id}")
 	public User deleteUser(@PathVariable long id) throws PgsqlUserBusinessException, PgsqlProfileBusinessException, PgsqlAddressBusinessException {
 		return dao.deleteUser(id);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value="/users/auth")
 	public User loginUser(@RequestParam String username, @RequestParam String password) throws PgsqlUserBusinessException, PgsqlProfileBusinessException, PgsqlAddressBusinessException {
 		return dao.userLogin(username, password);
