15b4677c3726803f1c2e5f5c45de1f44baaebd24
Bibek Lama <netshering@gmail.com@users.noreply.github.com>
2020-09-04 14:17:44 +0200

    Update POM and Launcher

--- pgsql-micro-service/src/main/java/fr/epita/pgsql/launcher/PGSQLSpringbootLauncher.java
+++ pgsql-micro-service/src/main/java/fr/epita/pgsql/launcher/PGSQLSpringbootLauncher.java
@@ -2,10 +2,17 @@ package fr.epita.pgsql.launcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication(scanBasePackages="fr.epita")
-public class PGSQLSpringbootLauncher {
+public class PGSQLSpringbootLauncher extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PGSQLSpringbootLauncher.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PGSQLSpringbootLauncher.class, args);
